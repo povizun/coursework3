840cfad9e8c81adb5f68d0b48a7cc8da8ad0d09e
Futeki <Povizun@bk.ru>
2023-12-15 19:16:59 +0300

    Preparing for development

---
+++ Utils/Operation.py
@@ -0,0 +1,6 @@
+class Operation:
+    def __init__(self, dictionary):
+        pass
+
+    def __repr__(self):
+        pass
---
+++ Utils/functions.py
@@ -0,0 +1,10 @@
+import json
+
+
+def get_dict_of_operations(file: str):
+    pass
+
+
+def get_dict_of_last_operations(dictionary: dict):
+    pass
+
